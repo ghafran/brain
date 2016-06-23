502cb94bac67d8745a9f37535a433097c7c7dd4f
ghafran <ghafranabbas@gmail.com>
2016-06-23 00:27:54 -0400

    d

---
+++ evenodd.js
@@ -0,0 +1,24 @@
+var synaptic = require('synaptic');
+var network = new synaptic.Architect.Perceptron(32, 64, 32, 16, 8, 4, 2, 1);
+
+var trainer = new synaptic.Trainer(network);
+var trainingOptions = {
+    rate: 0.1,
+    iterations: 20000,
+    error: 0.005,
+};
+
+var learningRate = 0.3;
+for(var i = 0; i < 900; i++){
+    // trainer.train([{
+    //     input: [i * 0.0001],
+    //     output: [ ( i % 2 === 0 ? 1: 0) ]
+    // }], trainingOptions);
+    console.log(i, i * 0.0001, ( i % 2 === 0 ? 1: 0));
+    network.activate([i * 0.0001]);
+    network.propagate(learningRate, [( i % 2 === 0 ? 1: 0)]);
+}
+
+for(var i = 0; i < 10; i++){
+    console.log(network.activate([i * 0.0001])[0]);
+}
