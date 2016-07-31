7c50b3e56c9f9db30a4a1f36e53c0b69672cdcbe
ghafran <ghafranabbas@gmail.com>
2016-07-31 12:26:30 -0400

    back

---
+++ phrase.js
@@ -0,0 +1,40 @@
+var ABC = {
+  toAscii: function(bin) {
+    return bin.replace(/\s*[01]{8}\s*/g, function(bin) {
+      return String.fromCharCode(parseInt(bin, 2));
+  });
+  },
+  toBinary: function(str, spaceSeparatedOctets) {
+    return str.replace(/[\s\S]/g, function(str) {
+      str = ABC.zeroPad(str.charCodeAt().toString(2));
+      return !(1 == spaceSeparatedOctets ? str : str + ' ');
+  });
+  },
+  zeroPad: function(num) {
+    return '00000000'.slice(String(num).length) + num;
+  }
+};
+
+var synaptic = require('synaptic');
+
+var input = 8;
+var pool = 30;
+var output = 1;
+var connections = 30;
+var gates = 10;
+
+var network = new synaptic.Architect.Liquid(input, pool, output, connections, gates);
+
+var learningRate = 0.1;
+network.activate([0,1,1,0,1,0,0,0]);
+network.activate([0,1,1,0,0,1,0,1]);
+network.activate([0,1,1,0,1,1,0,0]);
+network.activate([0,1,1,0,1,1,0,0]);
+network.activate([0,1,1,0,1,1,1,1]);
+network.propagate(learningRate, [1]);
+
+network.activate([0,1,1,0,1,0,0,0]);
+network.activate([0,1,1,0,0,1,0,1]);
+network.activate([0,1,1,0,1,1,0,0]);
+network.activate([0,1,1,0,1,1,0,0]);
+console.log(network.activate([0,1,1,0,1,1,1,1]));
